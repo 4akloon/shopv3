4eafd2e193770d69e51bd3f7765663fb54485c08
alexxx16386 <alexxx16386@gmail.com>
2021-01-19 00:40:39 +0200

    lol

--- shop/shop/prod_settings.py
+++ shop/shop/prod_settings.py
@@ -12,10 +12,10 @@ ALLOWED_HOSTS = ['192.168.0.2', '192.168.0.3', 'app-store.ks.ua', '0.0.0.0', '18
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dcm6kmhhotcg9v',
-        'USER': 'wsqyarnhexybcm',
-        'PASSWORD': '29a8c99b1155a1f10ecd134b274e2bfced70ea1cdd52ee228023896ae273a389',
-        'HOST': 'ec2-54-228-209-117.eu-west-1.compute.amazonaws.com',
+        'NAME': 'store',
+        'USER': 'appstoredb',
+        'PASSWORD': 'appstore228',
+        'HOST': 'local',
         'PORT': '5432',
     }
 }
